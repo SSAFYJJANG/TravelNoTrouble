1df116e63ab8b16c04b997c64f04a46b4ab0f0c7
jshinkor00 <jshinkor00@naver.com>
2024-05-14 15:45:37 +0900

    feat: add plan write page

--- enjoytrip_vue/src/router/index.js
+++ enjoytrip_vue/src/router/index.js
@@ -14,6 +14,7 @@ import HotplaceFeed from "@/components/hotplace/HotplaceFeed.vue";
 import HotplaceWrite from "@/components/hotplace/HotplaceWrite.vue";
 import MyTripPlanView from "@/views/MyTripPlanView.vue";
 import MyTripPlanList from "@/components/myTripPlan/MyTripPlanList.vue";
+import MyTripPlanWrite from "@/components/myTripPlan/MyTripPlanWrite.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -102,6 +103,11 @@ const router = createRouter({
           name: "plan-list",
           component: MyTripPlanList,
         },
+        {
+          path: "write",
+          name: "plan-write",
+          component: MyTripPlanWrite,
+        }
       ],
     },
   ],

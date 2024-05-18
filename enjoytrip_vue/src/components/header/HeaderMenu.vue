e4b0edfbac9e49e997bfb5f3d85266077b0a2aff
aa001R <areumk163@gmail.com>
2024-05-19 03:02:23 +0900

    refactor : modify header menu items

--- enjoytrip_vue/src/components/header/HeaderMenu.vue
+++ enjoytrip_vue/src/components/header/HeaderMenu.vue
@@ -1,24 +1,20 @@
 <script setup>
-import { ref, defineProps, defineEmits } from "vue";
+import { ref} from "vue";
 const login = ref({
   id: "",
   name: "",
   isLogin: true,
+  pofile: ""
 });
 </script>
 
-<template>
-  <button
-    class="navbar-toggler"
-    type="button"
-    data-toggle="collapse"
-    data-bs-toggle="collapse"
-    data-target="#navbarSupportedContent"
-    data-bs-target="#navbarSupportedContent"
+<template class="navbar navbar-expand-lg navbar-light bg-light">
+  <button class="navbar-toggler" 
+    type="button" data-toggle="collapse" data-bs-toggle="collapse"
+    data-target="#navbarSupportedContent" 
+    data-bs-target="#navbarSupportedContent" 
     aria-controls="navbarNavAltMarkup"
-    aria-expanded="false"
-    aria-label="Toggle navigation"
-  >
+    aria-expanded="false" aria-label="Toggle navigation">
     <div class="hamburger">
       <span></span>
       <span></span>
@@ -27,95 +23,61 @@ const login = ref({
     </div>
   </button>
 
-  <div
-    class="collapse navbar-collapse opacityScroll"
-    id="navbarSupportedContent"
-  >
+  <div class="collapse navbar-collapse opacityScroll" id="navbarSupportedContent">
     <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true">
       <li class="nav-item">
-        <router-link
-          class="nav-link link text-black display-4"
-          :to="{ name: 'spot' }"
-        >
-          관광지 조회
+        <router-link class="nav-link link text-black display-4" :to="{ name: 'board' }">
+          공지사항
         </router-link>
       </li>
       <li class="nav-item">
-        <router-link
-          class="nav-link link text-black display-4"
-          :to="{ name: 'hotplace' }"
-        >
-          핫플 구경 & 자랑하기
+        <router-link class="nav-link link text-black display-4" :to="{ name: 'hotplace' }">
+          인기 리뷰
         </router-link>
       </li>
       <li class="nav-item">
-        <router-link
-          class="nav-link link text-black display-4"
-          :to="{ name: 'plan' }"
-        >
+        <router-link class="nav-link link text-black display-4" :to="{ name: 'spot' }">
           나의 여행 계획
         </router-link>
       </li>
       <li class="nav-item dropdown">
-        <router-link
-          class="nav-link link text-black dropdown-toggle display-4"
-          :to="{ name: 'home' }"
-          data-toggle="dropdown-submenu"
-          data-bs-toggle="dropdown"
-          data-bs-auto-close="outside"
-          aria-expanded="false"
-        >
+        <div class="nav-link link text-black dropdown-toggle display-4" data-toggle="dropdown-submenu"
+          data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
           커뮤니티
-        </router-link>
+        </div>
+
         <div class="dropdown-menu" aria-labelledby="dropdown-694">
-          <router-link
-            class="text-black dropdown-item display-4"
-            :to="{ name: 'board' }"
-          >
-            공지사항<br />
-          </router-link>
-          <router-link
-            class="text-black dropdown-item display-4"
-            :to="{ name: 'board' }"
-          >
+          <router-link class="text-black dropdown-item display-4" :to="{ name: 'board' }">
             QnA<br />
           </router-link>
-          <router-link
-            class="text-black dropdown-item display-4"
-            :to="{ name: 'board' }"
-          >
+          <router-link class="text-black dropdown-item display-4" :to="{ name: 'board' }">
             자유게시판<br />
           </router-link>
         </div>
       </li>
-      <div v-if="login.isLogin">
-        <li class="nav-item dropdown">
-          <router-link
-            class="nav-link link text-black dropdown-toggle display-4"
-            :to="{ name: 'home' }"
-            data-toggle="dropdown-submenu"
-            data-bs-toggle="dropdown"
-            data-bs-auto-close="outside"
-            aria-expanded="false"
-          >
-            😄
+      <li v-if="login.isLogin" class="nav-item dropdown">
+        <div 
+          class="nav-link link text-black dropdown-toggle display-4" 
+          data-toggle="dropdown-submenu"
+          data-bs-toggle="dropdown" 
+          data-bs-auto-close="outside" 
+          aria-expanded="false">
+          <img v-if="login.pofile" src="" alt="">
+          <i v-else class="bi bi-person-circle"></i>
+        </div>
+        <div class="dropdown-menu" 
+          aria-labelledby="dropdown-694">
+          <router-link class="text-black dropdown-item display-4" 
+          :to="{ name: 'mypage' }">
+            마이페이지<br />
           </router-link>
-          <div class="dropdown-menu" aria-labelledby="dropdown-694">
-            <router-link
-              class="text-black dropdown-item display-4"
-              :to="{ name: 'mypage' }"
-            >
-              마이페이지<br />
-            </router-link>
-            <router-link
-              class="text-black dropdown-item display-4"
-              :to="{ name: 'home' }"
-            >
-              로그아웃<br />
-            </router-link>
-          </div>
-        </li>
-      </div>
+          <router-link 
+            class="text-black dropdown-item display-4" 
+            :to="{ name: 'home' }">
+            로그아웃<br />
+          </router-link>
+        </div>
+      </li>
     </ul>
   </div>
 </template>

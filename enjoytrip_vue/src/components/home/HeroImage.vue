f09bfc7b15fb51313e03a32d1b710841d95c38bd
aa001R <areumk163@gmail.com>
2024-05-19 02:55:52 +0900

    remove : attraction search button

--- enjoytrip_vue/src/components/home/HeroImage.vue
+++ enjoytrip_vue/src/components/home/HeroImage.vue
@@ -12,18 +12,13 @@ const subtitle = ref("Good Travel No Trouble");
 		id="header17-1l">
 		<div class="container-fluid">
 			<div class="row">
-				<div class="content-wrap col-12 col-md-12">
+				<div class="content-wrap col-12 col-md-12" >
 					<h1 class="mbr-section-title mbr-fonts-style mbr-white mb-4 display-1">
 						<strong>{{ maintitle }}</strong>
 					</h1>
 					<p class="mbr-fonts-style mbr-text mbr-white mb-4 display-7">
 						{{ subtitle }}&nbsp;
 					</p>
-					<div class="mbr-section-btn">
-						<router-link class="btn btn-primary-outline display-7" :to="{ name: 'spot' }">
-							관광지 조회하기
-						</router-link>
-					</div>
 				</div>
 			</div>
 		</div>

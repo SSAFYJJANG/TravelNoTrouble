24c4a17fdebcc7ba0b52d1dbb28868cfd5217ab1
aa001R <areumk163@gmail.com>
2024-05-19 03:12:29 +0900

    feat : add carousel for city/district attraction

---
+++ enjoytrip_vue/src/components/home/AreaAttraction.vue
@@ -0,0 +1,67 @@
+<script setup>
+import router from '@/router';
+import { ref } from 'vue';
+defineProps({
+    popularHotPlace: Object
+})
+
+const clickMove = () => {
+    router.push({ name: 'spot' });
+}
+
+</script>
+
+<template>
+    <div class="embla__slide slider-image item" 
+        style="margin-left: 0.25rem; margin-right: 0.25rem;">
+        <div class="slide-content">
+            <div class="item-img">
+                <div class="item-wrapper">
+                    <img @dblclick="clickMove" 
+                        :src=popularHotPlace.imgSrc 
+                        :alt=popularHotPlace.alt
+                        :title=popularHotPlace.title >
+                        <span class="img-txt-title">
+                            {{ popularHotPlace.sido }}
+                        </span>
+                        <span class="img-txt">
+                            -> 관광지 조회 하기
+                        </span>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.item-wrapper{
+    background-color: black;
+    border-radius: 0;
+}
+
+img {
+    border-radius: 0;
+    min-width: 250px;
+    max-width: 250px;
+    max-height: 400px;
+    min-height: 400px;
+    object-fit: cover;
+    opacity: 0.7;
+}
+.img-txt, .img-txt-title{
+    position : absolute;
+    z-index: 1;
+    color : white;
+}
+.img-txt-title{
+    font-size: 2rem;
+    font-weight: 500;
+    top : 2rem;
+    left : 2rem;
+}
+.img-txt{
+    bottom : 2rem;
+    right : 1rem;
+    opacity: 0.7;
+}
+</style>
---
+++ enjoytrip_vue/src/components/home/AreaAttractionList.vue
@@ -0,0 +1,142 @@
+<script setup>
+import { ref, computed, watch } from 'vue';
+import AreaAttraction from './AreaAttraction.vue';
+let id = 1;
+const imgsrc = "/src/assets/images/trip2.jpeg"
+// TODO : dataSlideTo, dataBsSlideTo은 객체마다 숫자가 달라야하는지, 
+// 숫자는 순차적으로 나와야하는지 확인
+const popularHotPlaces = ref([
+    {
+        no: id,
+        sido : "서울",
+        imgSrc: imgsrc,
+        alt: "",
+        title: "",
+        dataSlideTo: id,
+        dataBsSlideTo: id++,
+    },
+    {
+        no: id,
+        sido : "서울",
+        imgSrc: imgsrc,
+        alt: "",
+        title: "",
+        dataSlideTo: id,
+        dataBsSlideTo: id++,
+    },
+    {
+        no: id,
+        sido : "서울",
+        imgSrc: imgsrc,
+        alt: "",
+        title: "",
+        dataSlideTo: id,
+        dataBsSlideTo: id++,
+    },
+    {
+        no: id,
+        sido : "서울",
+        imgSrc: imgsrc,
+        alt: "",
+        title: "",
+        dataSlideTo: id,
+        dataBsSlideTo: id++,
+    },
+    {
+        no: id,
+        sido : "서울",
+        imgSrc: imgsrc,
+        alt: "",
+        title: "",
+        dataSlideTo: id,
+        dataBsSlideTo: id++,
+    },
+    {
+        no: id,
+        sido : "서울",
+        imgSrc: imgsrc,
+        alt: "",
+        title: "",
+        dataSlideTo: id,
+        dataBsSlideTo: id++,
+    },
+    {
+        no: id,
+        sido : "서울",
+        imgSrc: imgsrc,
+        alt: "",
+        title: "",
+        dataSlideTo: id,
+        dataBsSlideTo: id++,
+    },
+    {
+        no: id,
+        sido : "서울",
+        imgSrc: imgsrc,
+        alt: "",
+        title: "",
+        dataSlideTo: id,
+        dataBsSlideTo: id++,
+    },
+]);
+
+const lengthOfPopularHotPlaces = computed(() => {
+    return popularHotPlaces.value.length;
+})
+
+</script>
+
+<template>
+    <section data-bs-version="5.1" 
+        class="features017 mbr-embla cid-uc9PmWNUs6" id="features017-2n">
+        <div class="position-relative">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="mbr-section-head mb-4 mt-4">
+                            <h3 class="mbr-section-subtitle mbr-fonts-style ">
+                                지역별 여행지
+                            </h3>
+                            <hr style="margin: auto 12%;">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div v-if="lengthOfPopularHotPlaces" 
+                class="embla" 
+                data-skip-snaps="true" 
+                data-align="center"
+                data-contain-scroll="trimSnaps" 
+                data-loop="true" 
+                data-auto-play="true" 
+                data-auto-play-interval="2.5"
+                data-draggable="true">
+                
+                <div class="embla__viewport container-fluid">
+                    <div class="embla__container">
+                        <AreaAttraction v-for="popularHotPlace
+                            in popularHotPlaces" :key="popularHotPlace.no" :popular-hot-place="popularHotPlace" />
+                    </div>
+                </div>
+
+                <button class="embla__button embla__button--prev" style="display: none;">
+                    <span class="mobi-mbri mobi-mbri-arrow-prev mbr-iconfont" aria-hidden="true">
+                        <i class="bi bi-caret-left-fill"></i>
+                    </span>
+                    <span class="sr-only visually-hidden visually-hidden visually-hidden">Previous</span>
+                </button>
+                <button class="embla__button embla__button--next"  style="display: none;">
+                    <span class="mobi-mbri mobi-mbri-arrow-next mbr-iconfont" aria-hidden="true">
+                        <i class="bi bi-caret-right-fill"></i>
+                    </span>
+                    <span class="sr-only visually-hidden visually-hidden visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+
+</style>

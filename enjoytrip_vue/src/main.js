4053fd2cb806170a33bbd270665d642b81c35210
aa001R <areumk163@gmail.com>
2024-05-20 05:51:25 +0900

    chore: import additional Kakao Map API methods in main.js

--- enjoytrip_vue/src/main.js
+++ enjoytrip_vue/src/main.js
@@ -7,7 +7,7 @@ import "@vueup/vue-quill/dist/vue-quill.snow.css";
 
 // 카카오 지도
 import { useKakao } from "vue3-kakao-maps/@utils";
-useKakao("ff56269720c5ea3e30bdf3c3ad4fcb6d");
+useKakao('api-key', ['clusterer', 'services', 'drawing']); // 각 라이브러리는 ',(콤마)'로 구분합니다.
 
 // bootstrap vue
 import { BootstrapVue3 } from "bootstrap-vue-3";
@@ -17,6 +17,9 @@ import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 const app = createApp(App);
 app.component("QuillEditor", QuillEditor);
+
+//coreui
+// import '@coreui/coreui/dist/css/coreui.min.css'
 
 //AOS
 import AOS from 'aos';

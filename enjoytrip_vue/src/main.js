1cb6fb710213b563b387ef4ef016714914c98522
jjsshhhhh <jshinkor00@naver.com>
2024-05-19 18:35:25 +0900

    feat: add module pinia

--- enjoytrip_vue/src/main.js
+++ enjoytrip_vue/src/main.js
@@ -1,6 +1,8 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
+import piniaPluginPersistedstate from "pinia-plugin-persistedstate";
 
 import { QuillEditor } from "@vueup/vue-quill";
 import "@vueup/vue-quill/dist/vue-quill.snow.css";
@@ -16,8 +18,12 @@ import "bootstrap-vue-3/dist/bootstrap-vue-3.css";
 // icon
 import "bootstrap-icons/font/bootstrap-icons.css";
 const app = createApp(App);
+const pinia = createPinia();
+
 app.component("QuillEditor", QuillEditor);
 
+pinia.use(piniaPluginPersistedstate);
+app.use(pinia);
 app.use(router);
 app.use(BootstrapVue3);
 

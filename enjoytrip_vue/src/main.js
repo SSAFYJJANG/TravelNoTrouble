ccf3d3bea1decd694e364bbc9f1906fba7d6f49b
aa001R <areumk163@gmail.com>
2024-05-21 06:45:24 +0900

    chore : install VCalendar package

--- enjoytrip_vue/src/main.js
+++ enjoytrip_vue/src/main.js
@@ -9,7 +9,7 @@ import "@vueup/vue-quill/dist/vue-quill.snow.css";
 
 // 카카오 지도
 import { useKakao } from "vue3-kakao-maps/@utils";
-useKakao('api-key', ['clusterer', 'services', 'drawing']); // 각 라이브러리는 ',(콤마)'로 구분합니다.
+useKakao("ff56269720c5ea3e30bdf3c3ad4fcb6d", ['clusterer', 'services', 'drawing']); // 각 라이브러리는 ',(콤마)'로 구분합니다.
 
 // bootstrap vue
 import { BootstrapVue3 } from "bootstrap-vue-3";
@@ -28,6 +28,13 @@ app.component("QuillEditor", QuillEditor);
 //AOS
 import AOS from 'aos';
 import "aos/dist/aos.css";
+
+//V-Calendar
+import VCalendar from 'v-calendar';
+import 'v-calendar/style.css';
+// Use plugin with optional defaults
+app.use(VCalendar, {})
+
 app.use(AOS);
 pinia.use(piniaPluginPersistedstate);
 app.use(pinia);

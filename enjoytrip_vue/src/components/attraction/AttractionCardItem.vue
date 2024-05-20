9099d2b95c1658fa96b540869de355cf00613f57
aa001R <areumk163@gmail.com>
2024-05-21 06:47:14 +0900

    feat : Vue search Attraction result list

--- enjoytrip_vue/src/components/attraction/AttractionCardItem.vue
+++ enjoytrip_vue/src/components/attraction/AttractionCardItem.vue
@@ -1,13 +1,58 @@
 <script setup>
+import { CAccordion, CAccordionItem, CButton, CAccordionHeader, CAccordionBody } from '@coreui/vue';
 
+defineProps({ attraction: Object });
 </script>
 
 <template>
-    <div>
-        
+    <div class="card" style="width: 100%">
+        <img class="card-img-top" :src="attraction.first_image2" alt="Card image cap">
+        <div class="card-body">
+            <div class="card-btn d-flex flex-nowrap align-items-center justify-content-between">
+                <h5 class="card-title">{{ attraction.title }}</h5>
+                <div class="card-btn">
+                    <i class="bi bi-patch-plus"></i>
+                    <i class="bi bi-heart"></i>
+                </div>
+            </div>
+            <p class="card-text">{{ attraction.add1 }}</p>
+        </div>
+        <CAccordion flush>
+            <CAccordionItem>
+                <CAccordionHeader>
+                    <strong>자세히 보기</strong>
+                </CAccordionHeader>
+                <CAccordionBody>
+                    {{ attraction.overview }}
+                </CAccordionBody>
+            </CAccordionItem>
+        </CAccordion>
     </div>
 </template>
 
 <style scoped>
+.card {
+    /* border: 1px solid black; */
+    border-radius: 2%;
+    padding: 0.25rem;
+    margin : 1rem auto;
+}
 
+.card-title {
+    font-weight: bold;
+}
+
+.card-text {
+    color: #485664;
+}
+
+.card-img-top {
+    height: 200px;
+    width: 350px;
+    object-fit: cover;
+    align-self: center;
+}
+.card-btn i{
+    margin-left : 1rem;
+}
 </style>
--- enjoytrip_vue/src/components/attraction/AttractionCardList.vue
+++ enjoytrip_vue/src/components/attraction/AttractionCardList.vue
@@ -1,15 +1,26 @@
 <script setup>
-
+import {ref } from 'vue';
+import AttractionCardItem from './AttractionCardItem.vue';
+import data from "@/data/index.js";
+const attractionList = ref([]);
+attractionList.value = data.attractionList;
+console.log(attractionList);
 </script>
 
 <template>
     <div>
-        
+        <p class="info"><i class="bi bi-info-circle"></i> +를 누르면 여행계획에 추가할 수 있어요</p>
+        <AttractionCardItem v-for="attraction in attractionList" :key="attraction.content_id" :attraction="attraction"/>
+        <!-- <AttractionCardItem/> -->
     </div>
 </template>
 
 <style scoped>
-
+.info{
+    font-size: smaller;
+    color : #485664;
+    margin : 0.01rem;
+}
 </style>
 <!-- ㄴ  검색 결과 카드 리스트-->
     <!-- ㄴ 검색 결과 카드 아이템 리스트 

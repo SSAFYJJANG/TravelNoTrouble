c38f5a988eebca3596a9627d261858dc2d319cec
jshinkor00 <jshinkor00@naver.com>
2024-05-21 13:39:45 +0900

    fix: axios json charset

--- enjoytrip_vue/src/util/http-commons.js
+++ enjoytrip_vue/src/util/http-commons.js
@@ -18,6 +18,9 @@ const { VITE_VUE_API_URL } = import.meta.env;
 function localAxios() {
   const instance = axios.create({
     baseURL: VITE_VUE_API_URL,
+    headers: {
+      "Content-Type": "application/json;charset=utf-8",
+    },
     // withCredentials: true,
     // headers: {
     //   "Content-Type": "application/json;charset=utf-8",

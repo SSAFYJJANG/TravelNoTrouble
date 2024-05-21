58e5362f8cc6e7b6acf7efd1860010d0053e9a9d
aa001R <areumk163@gmail.com>
2024-05-21 17:41:29 +0900

    design : form-control, text input design

--- enjoytrip_vue/src/main.js
+++ enjoytrip_vue/src/main.js
@@ -9,7 +9,7 @@ import "@vueup/vue-quill/dist/vue-quill.snow.css";
 
 // 카카오 지도
 import { useKakao } from "vue3-kakao-maps/@utils";
-useKakao("ff56269720c5ea3e30bdf3c3ad4fcb6d", ['clusterer', 'services', 'drawing']); // 각 라이브러리는 ',(콤마)'로 구분합니다.
+useKakao("ff56269720c5ea3e30bdf3c3ad4fcb6d", ["clusterer", "services", "drawing"]); // 각 라이브러리는 ',(콤마)'로 구분합니다.
 
 // bootstrap vue
 import { BootstrapVue3 } from "bootstrap-vue-3";
@@ -26,14 +26,14 @@ app.component("QuillEditor", QuillEditor);
 // import '@coreui/coreui/dist/css/coreui.min.css'
 
 //AOS
-import AOS from 'aos';
+import AOS from "aos";
 import "aos/dist/aos.css";
 
 //V-Calendar
-import VCalendar from 'v-calendar';
-import 'v-calendar/style.css';
+import VCalendar from "v-calendar";
+import "v-calendar/style.css";
 // Use plugin with optional defaults
-app.use(VCalendar, {})
+app.use(VCalendar, {});
 
 app.use(AOS);
 pinia.use(piniaPluginPersistedstate);

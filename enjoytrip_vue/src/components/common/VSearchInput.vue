8f428b871df3ea58d8cd3277fa76cfbdf52048db
aa001R <areumk163@gmail.com>
2024-05-22 22:16:32 +0900

    feat : add ClickEnvent VsearchInput button

--- enjoytrip_vue/src/components/common/VSearchInput.vue
+++ enjoytrip_vue/src/components/common/VSearchInput.vue
@@ -2,13 +2,21 @@
 import { CFormInput, CForm, CButton, CInputGroup } from '@coreui/vue';
 import { ref } from 'vue';
 const props = defineProps({ btnColor: String });
+const emit = defineEmits(["clickBtn"]);
+const keyword = ref("");
+const clickBtn = () => {
+  console.log("click " + keyword.value);
+  emit("clickBtn", keyword.value);
+};
+
 </script>
 
 <template>
     <CForm class="d-flex justify-content-between">
         <CFormInput style="min-height: 33px; border-radius: 5px 0 0 5px;" type="search"
-            class="h-100 p-1 ms-1 search-input" placeholder="검색 키워드를 입력해주세요" />
-        <CButton type="button" class="m-0 me-1 search-button" :color="btnColor" variant="outline">검색</CButton>
+            class="h-100 p-1 ms-1 search-input" placeholder="검색 키워드를 입력해주세요" 
+            v-model="keyword"/>
+        <CButton type="button" class="m-0 me-1 search-button" :color="btnColor" variant="outline" @click="clickBtn">검색</CButton>
     </CForm>
 </template>
 

8f823f0c38c68365f5929d9e3daca10b4ee5d63f
aa001R <areumk163@gmail.com>
2024-05-22 17:57:28 +0900

    refactor : update axois & pinia attraction

--- enjoytrip_vue/src/components/attraction/SearchOffCanvas.vue
+++ enjoytrip_vue/src/components/attraction/SearchOffCanvas.vue
@@ -1,7 +1,6 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import AttractionSearch from '@/components/attraction/AttractionSearch.vue';
-import AttractionCardList from '@/components/attraction/AttractionCardList.vue';
 import { COffcanvas, CButton, CCloseButton, COffcanvasHeader, COffcanvasTitle, COffcanvasBody, } from '@coreui/vue';
 
 const visibleOffCanvas = ref(false);
@@ -9,17 +8,17 @@ const visibleOffCanvas = ref(false);
 
 <template>
     <div>
-        <CButton class="offcanvas-button" 
-            :class="visibleOffCanvas?'open':''" 
+        <CButton class="offcanvas-button" :class="visibleOffCanvas ? 'open' : ''"
             @click="() => { visibleOffCanvas = !visibleOffCanvas }">
             <i class="bi" :class="visibleOffCanvas ? 'bi-caret-left-fill' : 'bi-caret-right-fill'"></i>
         </CButton>
 
-        <COffcanvas class="search-off-canvas" :backdrop="false" placement="start" 
-            scroll :visible="visibleOffCanvas"
+        <COffcanvas class="search-off-canvas" :backdrop="false" placement="start" scroll :visible="visibleOffCanvas"
             @hide="() => { visibleOffCanvas = !visibleOffCanvas }">
             <COffcanvasHeader>
-                <COffcanvasTitle>관광지 조회</COffcanvasTitle>
+                <COffcanvasTitle>
+                    <i class="fa-solid fa-map-location-dot fa-bounce" style="color: #464344;"></i> 관광지 조회
+                </COffcanvasTitle>
             </COffcanvasHeader>
             <hr>
             <COffcanvasBody>
@@ -33,24 +32,24 @@ const visibleOffCanvas = ref(false);
 </template>
 
 <style scoped>
-.offcanvas-button{
-    margin:0;
+.offcanvas-button {
+    margin: 0;
     padding: 12px 1px;
     position: absolute;
-    top : calc(40vh);
+    top: calc(40vh);
     background-color: white;
-    left:398px;
+    left: 398px;
     transform: translateX(-400px);
-    transition : transform 0.3s ease-in-out;
+    transition: transform 0.3s ease-in-out;
     border-radius: 0 0.2rem 0.2rem 0;
     z-index: 200;
 }
 
-hr{
-    margin : 0;
+hr {
+    margin: 0;
 }
 
-.open{
+.open {
     transform: none;
 }
 </style>
--- enjoytrip_vue/src/stores/attraction.js
+++ enjoytrip_vue/src/stores/attraction.js
@@ -1,4 +1,4 @@
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import { defineStore } from "pinia";
 
 import {
@@ -13,140 +13,148 @@ import {
 } from "@/api/attraction";
 import { httpStatusCode } from "@/util/http-status";
 
-export const useAttractionStore = defineStore("AttractionStore", () => {
-  const sidoList = ref(null);
-  const gugunList = ref(null);
-  const searchAttractionList = ref(null);
-  const attractionCnt = ref(2357);
-  const recommendationAttraction = ref(null);
-  const likeAttractionList = ref(null);
+export const useAttractionStore = defineStore(
+  "AttractionStore",
+  () => {
+    const sidoList = ref(null);
+    const gugunList = ref(null);
+    const searchAttractionList = ref(null);
+    const attractionCnt = ref(2357);
+    const recommendationAttraction = ref(null);
+    const likeAttractionList = ref(null);
+    const isSelectSidoCode = ref(0);
 
-  const getSidoList = async () => {
-    await sido(
-      (response) => {
-        if (response.status === httpStatusCode.OK) {
-          console.log("response", response);
-          sidoList.value = response.data;
-          console.log("sidoList", response.data);
+    const getSidoList = async () => {
+      await sido(
+        (response) => {
+          if (response.status === httpStatusCode.OK) {
+            sidoList.value = response.data;
+            console.log("sidoList", sidoList.value);
+          }
+        },
+        async (error) => {
+          console.error("get sido list", error);
         }
-      },
-      async (error) => {
-        console.error("get sido list", error);
-      }
-    );
-  };
-  const getGugunList = async () => {
-    await gugun(
-      (response) => {
-        if (response.status === httpStatusCode.OK) {
-          console.log("response", response);
-          gugunList.value = response.data;
-          console.log("gugunList", response.data);
+      );
+    };
+    const getGugunList = async () => {
+      await gugun(
+        (response) => {
+          if (response.status === httpStatusCode.OK) {
+            gugunList.value = response.data;
+            console.log("gugunList", gugunList.value);
+          }
+        },
+        async (error) => {
+          console.error("get gugun list", error);
         }
-      },
-      async (error) => {
-        console.error("get gugun list", error);
-      }
-    );
-  };
+      );
+    };
 
-  const getAttractionList = async (data) => {
-    await attractionResult(
-      data,
-      (response) => {
-        if (response.status === httpStatusCode.OK) {
-          console.log("response", response);
-          searchAttractionList.value = response.data;
-          console.log("searchAttractionList", response.data);
+    const getAttractionList = async (data) => {
+      await attractionResult(
+        data,
+        (response) => {
+          if (response.status === httpStatusCode.OK) {
+            searchAttractionList.value = response.data;
+            console.log("searchAttractionList", response.data);
+          }
+        },
+        async (error) => {
+          console.error("get search Attraction list", error);
         }
-      },
-      async (error) => {
-        console.error("get search Attraction list", error);
-      }
-    );
-  };
-  const getTotalAttractionCount = async () => {
-    await totalAttractionCnt(
-      (response) => {
-        if (response.status === httpStatusCode.OK) {
-          console.log("response", response);
-          attractionCnt.value = response.data;
-          console.log("attractionCnt", response.data);
+      );
+    };
+    const getTotalAttractionCount = async () => {
+      await totalAttractionCnt(
+        (response) => {
+          if (response.status === httpStatusCode.OK) {
+            attractionCnt.value = response.data;
+            console.log("attractionCnt", response.data);
+          }
+        },
+        async (error) => {
+          console.error("get attraction count", error);
         }
-      },
-      async (error) => {
-        console.error("get attraction count", error);
-      }
-    );
-  };
-  const getRecommendationAttraction = async () => {
-    await recommendedAttraction(
-      (response) => {
-        if (response.status === httpStatusCode.OK) {
-          console.log("response", response);
-          recommendationAttraction.value = response.data;
-          console.log("recommendationAttraction", response.data);
+      );
+    };
+    const getRecommendationAttraction = async () => {
+      await recommendedAttraction(
+        (response) => {
+          if (response.status === httpStatusCode.OK) {
+            recommendationAttraction.value = response.data;
+            console.log("recommendationAttraction", response.data);
+          }
+        },
+        async (error) => {
+          console.error("get recommendation Attraction", error);
         }
-      },
-      async (error) => {
-        console.error("get recommendation Attraction", error);
-      }
-    );
-  };
-  const getUserAttractionList = async (data) => {
-    await userLikeAttraction(
-      data,
-      (response) => {
-        if (response.status === httpStatusCode.OK) {
-          console.log("response", response);
-          likeAttractionList.value = response.data;
-          console.log("likeAttractionList", response.data);
+      );
+    };
+    const getUserAttractionList = async (data) => {
+      await userLikeAttraction(
+        data,
+        (response) => {
+          if (response.status === httpStatusCode.OK) {
+            likeAttractionList.value = response.data;
+            console.log("likeAttractionList", response.data);
+          }
+        },
+        async (error) => {
+          console.error("get like Attraction List", error);
         }
-      },
-      async (error) => {
-        console.error("get like Attraction List", error);
-      }
-    );
-  };
-  const insertAttraction = async (data) => {
-    await addLikeAttraction(
-      data,
-      (response) => {
-        if (response.status === httpStatusCode.OK) {
-          console.log("response", response);
-          console.log("insert Like Attraction", response.data);
-          getUserAttractionList();
+      );
+    };
+    const insertAttraction = async (data) => {
+      await addLikeAttraction(
+        data,
+        (response) => {
+          if (response.status === httpStatusCode.OK) {
+            console.log("insert Like Attraction", response.data);
+            getUserAttractionList();
+          }
+        },
+        async (error) => {
+          console.error("insert like Attraction List", error);
         }
-      },
-      async (error) => {
-        console.error("insert like Attraction List", error);
-      }
-    );
-  };
+      );
+    };
 
-  const deleteAttraction = async (data) => {
-    await deleteLikeAttraction(
-      data,
-      (response) => {
-        if (response.status === httpStatusCode.OK) {
-          console.log("response", response);
-          console.log("like Attraction List", response.data);
-          getUserAttractionList();
+    const deleteAttraction = async (data) => {
+      await deleteLikeAttraction(
+        data,
+        (response) => {
+          if (response.status === httpStatusCode.OK) {
+            console.log("response", response);
+            console.log("like Attraction List", response.data);
+            getUserAttractionList();
+          }
+        },
+        async (error) => {
+          console.error("delete like Attraction List", error);
         }
-      },
-      async (error) => {
-        console.error("delete like Attraction List", error);
-      }
-    );
-  };
-  return {
-    getSidoList,
-    getGugunList,
-    getAttractionList,
-    getTotalAttractionCount,
-    getRecommendationAttraction,
-    getUserAttractionList,
-    insertAttraction,
-    deleteAttraction,
-  };
-});
+      );
+    };
+
+    return {
+      isSelectSidoCode,
+      sidoList,
+      gugunList,
+      searchAttractionList,
+      attractionCnt,
+      recommendationAttraction,
+      likeAttractionList,
+      getSidoList,
+      getGugunList,
+      getAttractionList,
+      getTotalAttractionCount,
+      getRecommendationAttraction,
+      getUserAttractionList,
+      insertAttraction,
+      deleteAttraction,
+    };
+  },
+  {
+    persist: true,
+  }
+);

85c821e82164afcfae07c4d1aa1267015daeaf28
jjsshhhhh <jshinkor00@naver.com>
2024-05-23 00:12:18 +0900

    fix: PlanDetailDto - add fields

--- enjoytrip_springboot/src/main/java/com/trip/vue/plan/model/PlanDetailDto.java
+++ enjoytrip_springboot/src/main/java/com/trip/vue/plan/model/PlanDetailDto.java
@@ -8,4 +8,10 @@ public class PlanDetailDto {
 	private int plan_days_id;
 	private int content_id;
 	private String overview;
+	private String content_name;
+	private String title;
+	private String addr1;
+	private String addr2;
+	private String first_image;
+	private String first_image2;
 }

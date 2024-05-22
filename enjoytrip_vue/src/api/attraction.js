b2c11b925bf9aea87ffb52a49d9a37134b270a64
aa001R <areumk163@gmail.com>
2024-05-22 13:31:27 +0900

    feat : axois Attraction API

--- enjoytrip_vue/src/api/attraction.js
+++ enjoytrip_vue/src/api/attraction.js
@@ -1,9 +1,38 @@
 import { localAxios } from "@/util/http-commons";
 const local = localAxios();
 
-async function getAttractionCnt(success, fail) {
-    await local.get(`/best`).then(success).catch(fail);
+async function sido() {
+  await local.get(`/attraction/sido`).then(success).catch(fail);
+}
+async function gugun() {
+  await local.get(`/attraction/gugn`).then(success).catch(fail);
+}
+async function attractionResult(params) {
+  await local.get(`/attraction`, { params }).then(success).catch(fail);
+}
+async function totalAttractionCnt() {
+  await local.get(`/attraction/cnt`).then(success).catch(fail);
+}
+async function recommendedAttraction() {
+  await local.get(`/attraction/recommend`).then(success).catch(fail);
+}
+async function userLikeAttraction(userId) {
+  await local.get(`/attraction/cart/${userId}`).then(success).catch(fail);
+}
+async function addLikeAttraction(params) {
+  await local.post(`/attraction/cart`, { params }).then(success).catch(fail);
+}
+async function deleteLikeAttraction(params) {
+  await local.delete(`/attraction/cart`, { params }).then(success).catch(fail);
 }
 
-
-  
+export {
+  sido,
+  gugun,
+  attractionResult,
+  totalAttractionCnt,
+  recommendedAttraction,
+  userLikeAttraction,
+  addLikeAttraction,
+  deleteLikeAttraction,
+};

2a2d06587daeaeafed10382cd68d1fc75676c8a2
aa001R <areumk163@gmail.com>
2024-05-22 18:01:34 +0900

    refactor : update axios Main View

--- enjoytrip_vue/src/views/HomeView.vue
+++ enjoytrip_vue/src/views/HomeView.vue
@@ -1,11 +1,42 @@
 <script setup>
-import { onBeforeRouteUpdate, useRouter } from "vue-router";
+import { onMounted } from 'vue';
+import { onBeforeRouteUpdate, useRouter, } from "vue-router";
 import HeroImage from '@/components/home/HeroImage.vue';
 import CountDown from '@/components/home/CountDown.vue';
 import PopularHotPlacesList from '@/components/home/PopularHotPlacesList.vue';
 import Weather from "@/components/home/Weather.vue";
 import RecommendedAttraction from "@/components/home/RecommendedAttraction.vue";
 import AreaAttractionList from "@/components/home/AreaAttractionList.vue";
+import { useUserStore } from "@/stores/user";
+import { useHotplaceStore } from "@/stores/hotplace";
+import { useAttractionStore } from "@/stores/attraction";
+
+const userStore = useUserStore();
+const { getUsersCnt, userTotalCnt } = userStore;
+
+const hotplaceStore = useHotplaceStore();
+const { getTotalHotplaceCount, hotplaceCnt,
+    getBestHotplaceFeed, bestFeedList } = hotplaceStore;
+
+const attractionStore = useAttractionStore();
+const { getTotalAttractionCount, attractionCnt,
+    getRecommendationAttraction, recommendationAttraction,
+    getSidoList, sidoList } = attractionStore;
+
+// const attractionCnt = ref(571);
+
+
+onMounted(async () => {
+    await Promise.all([
+        getUsersCnt(),
+        getTotalHotplaceCount(),
+        getTotalAttractionCount(),
+        getRecommendationAttraction(),
+        getSidoList(),
+        getBestHotplaceFeed()
+    ]);
+});
+
 </script>
 
 
@@ -14,14 +45,14 @@ import AreaAttractionList from "@/components/home/AreaAttractionList.vue";
         <!-- 메인이미지&제목 -->
         <HeroImage />
         <!-- user & attractions countdown -->
-        <CountDown />
+        <CountDown :user-Cnt="userStore.userTotalCnt" :hotplace-cnt="hotplaceStore.hotplaceCnt"
+            :attraction-cnt="attractionStore.attractionCnt" />
         <Weather />
         <!-- 추천 & 지역 여행지 -->
-        <RecommendedAttraction />
-        <AreaAttractionList />
+        <RecommendedAttraction :recommendation-attraction="attractionStore.recommendationAttraction" />
+        <AreaAttractionList :sido-list="attractionStore.sidoList" />
         <!-- hotplace 인기글 -->
-        <PopularHotPlacesList 
-        />
+        <PopularHotPlacesList :best-feed-list="hotplaceStore.bestFeedList" />
     </div>
 </template>
 

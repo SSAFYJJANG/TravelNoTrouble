1e49030ad2c1b5f7b7ab913127d30d47a68dc3a7
jjsshhhhh <jshinkor00@naver.com>
2024-05-24 19:01:39 +0900

    remove: remove free board from header menu

--- enjoytrip_vue/src/components/header/HeaderMenu.vue
+++ enjoytrip_vue/src/components/header/HeaderMenu.vue
@@ -12,7 +12,7 @@ const login = ref({
   id: "",
   name: "",
   isLogin: true,
-  pofile: ""
+  pofile: "",
 });
 
 const clickLogout = () => {
@@ -22,9 +22,17 @@ const clickLogout = () => {
 </script>
 
 <template class="navbar navbar-expand-lg navbar-light bg-light">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-bs-toggle="collapse"
-    data-target="#navbarSupportedContent" data-bs-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup"
-    aria-expanded="false" aria-label="Toggle navigation">
+  <button
+    class="navbar-toggler"
+    type="button"
+    data-toggle="collapse"
+    data-bs-toggle="collapse"
+    data-target="#navbarSupportedContent"
+    data-bs-target="#navbarSupportedContent"
+    aria-controls="navbarNavAltMarkup"
+    aria-expanded="false"
+    aria-label="Toggle navigation"
+  >
     <div class="hamburger">
       <span></span>
       <span></span>
@@ -33,44 +41,61 @@ const clickLogout = () => {
     </div>
   </button>
 
-  <div class="collapse navbar-collapse opacityScroll" id="navbarSupportedContent">
+  <div
+    class="collapse navbar-collapse opacityScroll"
+    id="navbarSupportedContent"
+  >
     <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true">
       <li class="nav-item">
-        <router-link class="nav-link link text-black display-4" :to="{ name: 'board' }">
+        <router-link
+          class="nav-link link text-black display-4"
+          :to="{ name: 'board' }"
+        >
           공지사항
         </router-link>
       </li>
       <li class="nav-item">
-        <router-link class="nav-link link text-black display-4" :to="{ name: 'hotplace' }">
+        <router-link
+          class="nav-link link text-black display-4"
+          :to="{ name: 'hotplace' }"
+        >
           인기 리뷰
         </router-link>
       </li>
       <li class="nav-item">
-        <router-link class="nav-link link text-black display-4" :to="{ name: 'place' }">
+        <router-link
+          class="nav-link link text-black display-4"
+          :to="{ name: 'place' }"
+        >
           나의 여행 계획
         </router-link>
       </li>
-      <li class="nav-item">
-        <router-link class="nav-link link text-black display-4" :to="{ name: 'board' }">
-          자유게시판
-        </router-link>
-      </li>
       <li v-if="login.isLogin" class="nav-item dropdown">
-        <div class="nav-link link text-black dropdown-toggle display-4" data-toggle="dropdown-submenu"
-          data-bs-toggle="dropdown" data-bs-auto-close="outside" aria-expanded="false">
-          <img v-if="login.pofile" src="" alt="">
+        <div
+          class="nav-link link text-black dropdown-toggle display-4"
+          data-toggle="dropdown-submenu"
+          data-bs-toggle="dropdown"
+          data-bs-auto-close="outside"
+          aria-expanded="false"
+        >
+          <img v-if="login.pofile" src="" alt="" />
           <i v-else class="bi bi-person-circle"></i>
         </div>
         <div class="dropdown-menu" aria-labelledby="dropdown-694">
-          <router-link class="text-black dropdown-item display-4" :to="{ name: 'mypage' }">
+          <router-link
+            class="text-black dropdown-item display-4"
+            :to="{ name: 'mypage' }"
+          >
             마이페이지<br />
           </router-link>
-          <button class="text-black dropdown-item display-4" @click="clickLogout">
+          <button
+            class="text-black dropdown-item display-4"
+            @click="clickLogout"
+          >
             로그아웃<br />
           </button>
         </div>
       </li>
-
     </ul>
   </div>
 </template>
